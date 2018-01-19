e42b8dafd065230616f27210a2793ddd72779a13
ms2300 <matt.sewall@gmail.com>
2018-01-18 23:44:08 -0800

    Create world and character classes, addresses #8

---
+++ src/StreakerGame.java
@@ -0,0 +1,12 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class StreakerGame extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    public void start(Stage theStage) {
+        theStage.setTitle("Hello, World!");
+        theStage.show();
+    }
+}
